9374b392ee96029eb5a8de0dc95efbf32bd20d83
Ben LeVeque <ben.leveque@gmail.com>
2014-01-24 21:05:32 -0500

    added listener

--- popup.js
+++ popup.js
@@ -5,8 +5,20 @@ var Popup = (function() {
 	function load() {
 		console.log("Hello World!");
 		console.log(chrome.browsingData);
+		console.log(chrome.processes);
+		setUpListeners();
+	}
+
+	function setUpListeners() {
 		chrome.processes.onUpdatedWithMemory.addListener(function(processes) {
-			console.log(processes);
+			var i, id;
+			for(id in processes) {
+				if(processes.hasOwnProperty(id)) {
+					if(processes[id].network > 0) {
+						console.log(processes[id]);//"ID: " + process.osProcessId+", ")
+					}
+				}
+			}
 		});
 	}
 
