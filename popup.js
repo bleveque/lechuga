8ef742bf749ebdc6b10e65e30f72b6e2e6e4ab37
fastturtle <gabriel_barth-maron@alumni.brown.edu>
2014-01-24 23:02:27 -0500

    info retrieval works

--- popup.js
+++ popup.js
@@ -14,12 +14,44 @@ var Popup = (function() {
 			var i, id;
 			for(id in processes) {
 				if(processes.hasOwnProperty(id)) {
-					if(processes[id].network > 0) {
-						console.log(processes[id]);//"ID: " + process.osProcessId+", ")
-					}
+                    var proc = processes[id];
+                    get_proc_info(proc, function(info) {console.log("INFO!:", info)});
+					// if(processes[id].network > 0) {
+					// 	console.log(processes[id]);
+					// }
 				}
 			}
 		});
 	}
+
+    function get_proc_info(process, callback) {
+        var info = {};
+
+        if (process.tabs.length === 1) {
+            // This is a tab process
+            info["name"] = "tab";
+
+            var tabid = process.tabs[0];
+            info["tabid"] = tabid;
+
+            chrome.tabs.get(tabid,
+                function(tab) {
+                    info["title"] = tab.title;
+                    info["url"] = tab.url;
+                    callback(info);
+                });
+        }
+        else {
+            // Not a tab
+            info["name"] = process["type"];
+            callback(info);
+        }
+        // if (process.tabs.length > 0) {
+        //     console.log("TYPE:", process.type, "TABS:", process.tabs);
+        // }
+
+        return info;
+    }
+
 
 })();
